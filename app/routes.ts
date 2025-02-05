39f1bf848fd91670678d9d9b690fc6b5c3533fba
spazjibo <curtjibo@gmail.com>
2025-02-04 18:08:09 -0600

    Adding static blog mdx content and rollup.

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+// Adding route as this is only way I get this to work.
+export default [
+  index("routes/home.tsx"),
+  route("blog/first-blog", "routes/blog/first-blog.mdx"),
+] satisfies RouteConfig;
